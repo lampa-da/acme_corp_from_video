b4d664f4cd6c695a872b987ab2e412045f8ba913
Kate Sv <radistkatya@gmail.com>
2021-11-12 21:13:14 -0800

    refactor data layer

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const Sequelize = require('sequelize')
-const {STRING, UUID, UUIDV4} = Sequelize
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_corp_db')
+const {conn, syncAndSeed, models: {Department, Employee}} = require('./db')
 
 const express = require('express')
 const app = express()
@@ -43,49 +41,6 @@ app.get ('/api/employees', async(req, res, next)=>{
   }
 })
 
-const Department =conn.define('department', {
-  name: {
-    type: STRING(20),
-  }
-})
-
-const Employee =conn.define('employee', {
-  id: {
-    type: UUID,
-    primaryKey: true,
-    defaultValue: UUIDV4
-  },
-  name: {
-    type: STRING(20),
-  }
-})
-
-Department.belongsTo(Employee, { as: 'manager'})
-Employee.hasMany(Department, { foreignKey: 'managerId'})
-
-Employee.belongsTo(Employee, {as: 'supervisor'})
-Employee.hasMany(Employee, { foreignKey: 'supervisorId', as: 'supervisees'})
-
-const syncAndSeed = async()=>{
-  await conn.sync({force: true})
-  const [moe, lucy, larry, hr, engineering] = await Promise.all([
-    Employee.create({name: 'moe'}),
-    Employee.create({name: 'lucy'}),
-    Employee.create({name: 'larry'}),
-    Department.create({name: 'hr'}),
-    Department.create({name: 'engineering'})
-  ])
-
-  hr.managerId = lucy.id; // don't foreget to use an alias, not the original name of the table
-  await hr.save();
-  console.log(JSON.stringify(hr, null, 2)) //to get the info of specific table in readable form
-  moe.supervisorId = lucy.id
-  larry.supervisorId = lucy.id
-  await Promise.all([
-    moe.save(),
-    larry.save()
-  ])
-}
 
 const init = async()=>{
   try{
